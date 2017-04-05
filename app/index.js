43fe85837d48ae949a997ccc70b06db625ae7c1a
Matt Viggiano <matthewviggiano@gmail.com>
2017-04-05 00:26:17 -0500

    Finally got around to redirecting from base directory to index.

--- app/index.js
+++ app/index.js
@@ -1,33 +1,38 @@
-//app/index.js
-var http = require('http');
-var fs = require ('fs');
-var url = require('url');
-var port = process.env.PORT || 5000;
-// var coloring = require('../resources/coloring');
+// app/index.js
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
-http.createServer(function(req,res) {
-    var pathname = url.parse(req.url).pathname;
-    console.log("Request for " + pathname + " received.");
+const port = process.env.PORT || 5000;
 
-    // Read the requested file content from file system
-    fs.readFile("app/" + pathname.substr(1), function (err, data) {
-        if (err) {
-            console.log(err);
-            // HTTP Status: 404 : NOT FOUND
-            // Content Type: text/plain
-            res.writeHead(404, {'Content-Type': 'text/html'});
-        } else {
-            //Page found      
-            // HTTP Status: 200 : OK
-            // Content Type: text/plain
-            res.writeHead(200, {'Content-Type': 'text/html'}); 
-         
-            // Write the content of the file to response body
-            res.write(data.toString());
-            // coloring.init();
-        }
-        res.end();
-    });
+http.createServer((req, res) => {
+  const pathname = url.parse(req.url).pathname;
+  console.log(`Request for ${pathname} received.`);
+
+  if (pathname === '/' || !pathname || typeof pathname === 'undefined') {
+    console.log('Redirecting to index.');
+    res.writeHead(302, { location: 'index.html', 'Content-Type': 'text/html' });
+    res.end();
+  }
+
+  // Read the requested file content from file system
+  fs.readFile(`app/${pathname.substr(1)}`, (err, data) => {
+    if (err) {
+      console.log(err);
+      // HTTP Status: 404 : NOT FOUND
+      // Content Type: text/plain
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+    } else {
+      // Page found
+      // HTTP Status: 200 : OK
+      // Content Type: text/plain
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      // Write the content of the file to response body
+      res.write(data.toString());
+      // coloring.init();
+    }
+    res.end();
+  });
 }).listen(port);
 
 console.log('Server running on port %s', port);
